61bdcfc85efdb3f8b62c994d0d8ca94b44fb5557
Alexander Kaschuk <58186279+AlexKaschuk@users.noreply.github.com>
2020-01-20 16:55:27 +0200

    Add files via upload

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,92 +1,91 @@
-class Fighter {
-    constructor({ name, damage, hp, strength, agility }) {
-        this._name = name;
-        this._damage = damage;
-        this._hp = hp;
-        this._strength = strength;
-        this._agility = agility;
-        this._startHp = hp;
-        this._wins = 0;
-        this._loss = 0;
-    }
-    getName() {
-        return this._name;
-    }
-    getDamage() {
-        return this._damage;
-    }
-    getStrength() {
-        return this._strength;
-    }
-    getAgility() {
-        return this._agility;
-    }
-    getHealth() {
-        return this._hp;
-    }
-    attack(defendFighter) {
-        let atata = defendFighter.getStrength() + defendFighter.getAgility();
-        const MAX_CHANCE = 100;
-        if (Math.floor(Math.random() * MAX_CHANCE > atata)) {
-            console.log(
-                `${this._name} makes ${defendFighter.getName()}` +
-                ` ${this._damage} damage`
-            );
-            defendFighter.dealDamage(this._damage);
-        } else {
-            console.log(`${this._name} atack missed`);
+function Fighter(parameters) {
+    let _name = parameters.name;
+    let _damage = parameters.damage;
+    let _hp = parameters.hp;
+    let _strength = parameters.strength;
+    let _agility = parameters.agility;
+    let _startHp = parameters.hp;
+    let _wins = 0;
+    let _loss = 0;
+
+    return new class _Fighter {
+        getName() {
+            return _name;
         }
-    }
-    logCombatHistory() {
-        console.log(
-            `Name: ${this._name}, Wins: ${this._wins}, Loses: ${this._loss}`
-        );
-    }
-    heal(heals) {
-        if (this._hp > this._startHp - heals) {
-            this._hp = this._startHp;
-        } else {
-            this._hp += heals;
+        getDamage() {
+            return _damage;
         }
-    }
-    dealDamage(makedDamage) {
-        if (this._hp <= makedDamage) {
-            this._hp = 0;
-        } else {
-            this._hp = this._hp - makedDamage;
+        getStrength() {
+            return _strength;
         }
-    }
-    addWin() {
-        this._wins++;
-    }
-    addLoss() {
-        this._loss++;
-    }
+        getAgility() {
+            return _agility;
+        }
+        getHealth() {
+            return _hp;
+        }
+        attack(defendFighter) {
+            const atata = defendFighter.getStrength() + defendFighter.getAgility();
+            const MAX_CHANCE = 100;
+            if (Math.random() * MAX_CHANCE > atata) {
+                console.log(
+                    `${_name} makes ${defendFighter.getName()}` + ` ${_damage} damage`
+                );
+                defendFighter.dealDamage(_damage);
+            } else {
+                console.log(`${_name} atack missed`);
+            }
+        }
+        logCombatHistory() {
+            console.log(`Name: ${_name}, Wins: ${_wins}, Loses: ${_loss}`);
+        }
+        heal(heals) {
+            if (_hp > _startHp - heals) {
+                _hp = _startHp;
+            } else {
+                _hp += heals;
+            }
+        }
+        dealDamage(makedDamage) {
+            if (_hp <= makedDamage) {
+                _hp = 0;
+            } else {
+                _hp = _hp - makedDamage;
+            }
+        }
+        addWin() {
+            _wins++;
+        }
+        addLoss() {
+            _loss++;
+        }
+    }();
 }
 
 function battle(myFighter, myFighter1) {
-    if (myFighter.getHealth() === 0) {
+    if (!myFighter.getHealth()) {
         console.log(`${myFighter.getName()} is dead and can't fight`);
-    } else if (myFighter1.getHealth() === 0) {
+    } else if (!myFighter1.getHealth()) {
         console.log(`${myFighter1.getName()} is dead and can't fight`);
     } else {
-        if (myFighter.getHealth() > 0 && myFighter1.getHealth() > 0) {
+        while (myFighter.getHealth() && myFighter1.getHealth()) {
             myFighter.attack(myFighter1);
-            if (myFighter1.getHealth() > 0) {
+            if (myFighter1.getHealth()) {
                 myFighter1.attack(myFighter);
-                battle(myFighter, myFighter1);
-            } else {
-                console.log(`${myFighter.getName()} has won`);
-                myFighter1.addLoss();
-                myFighter.addWin();
             }
-        } else if (myFighter.getHealth() <= 0) {
+        }
+        if (myFighter.getHealth()) {
+            console.log(`${myFighter.getName()} has won`);
+            myFighter1.addLoss();
+            myFighter.addWin();
+        } else {
             console.log(`${myFighter1.getName()} has won`);
             myFighter.addLoss();
             myFighter1.addWin();
         }
     }
 }
+
 
 const myFighter = new Fighter({
     name: 'Maximus',
